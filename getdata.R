d33580f699bead998eba76280654474731ae977c
rabsjp <jeanpaulrab@gmail.com>
2018-11-04 14:00:32 +0000

    peru

--- getdata.R
+++ getdata.R
@@ -1,7 +1,7 @@
 rm(list = ls())
 setwd("/cloud/project/project/data/")
 files = list.files(pattern="etfs.*")
-filename<-files[1]
+filename<-files[2]
 df<-read.csv(filename,sep=",",header=T, stringsAsFactors = FALSE)
 
 important_names<-c(1,14:57)
@@ -12,16 +12,16 @@ df$type<-0
 df$type[df$participant.id_in_session>3]<-1
 df$type[df$participant.id_in_session>6]<-2
 df$typeperiod<-df$subsession.round_number*10+df$type
-df$portab<-(df$player.n_a-df$player.n_b)^2/3
-df$portdifab<-(df$player.n_a-df$player.n_b)/3
+df$portab<-(df$player.n_a+df$player.t_a-df$player.n_b-df$player.t_b)^2/3
+df$portdifab<-(df$player.n_a+df$player.t_a-df$player.n_b-df$player.t_b)/3
 df$portgroup<-ave(df$portab,df$subsession.round_number,FUN=function(x) sum(x, na.rm=T))
-df$agroup<-ave(df$player.n_a,df$typeperiod,FUN=function(x) sum(x, na.rm=T))
-df$bgroup<-ave(df$player.n_b,df$typeperiod,FUN=function(x) sum(x, na.rm=T))
-df$cgroup<-ave(df$player.n_c,df$typeperiod,FUN=function(x) sum(x, na.rm=T))
+df$agroup<-ave(df$player.n_a+df$player.t_a,df$typeperiod,FUN=function(x) sum(x, na.rm=T))
+df$bgroup<-ave(df$player.n_b+df$player.t_b,df$typeperiod,FUN=function(x) sum(x, na.rm=T))
+df$cgroup<-ave(df$player.n_c+df$player.t_c,df$typeperiod,FUN=function(x) sum(x, na.rm=T))
 df$portype<-ave(df$portab,df$typeperiod,FUN=function(x) sum(x, na.rm=T))
 df$portdiftype<-ave(df$portdifab,df$typeperiod,FUN=function(x) sum(x, na.rm=T))
 
-png("prices.png")
+png("prices1.png")
 plot(df$subsession.round_number,df$group.p_c,ylim=c(0,100),col="blue",xaxt = "n",xlab="period",ylab = "prices")
 axis(1, at=c(1:15,18:32), labels=c(1:15,1:15))
 lines(df$subsession.round_number,df$group.nav,col="blue")
@@ -31,7 +31,7 @@ points(df$subsession.round_number,df$group.p_b,col="red",pch=3)
 #lines(df$subsession.round_number,df$group.nav_b,col="red")
 dev.off()
 
-png("marketq.png")
+png("marketq1.png")
 plot(df$subsession.round_number,df$group.q_c,ylim=c(0,10),col="blue",type="l",xaxt = "n",xlab="period",ylab = "q")
 axis(1, at=c(1:15,18:32), labels=c(1:15,1:15))
 lines(df$subsession.round_number,df$group.q_a,col="gray")
@@ -39,42 +39,36 @@ lines(df$subsession.round_number,df$group.q_b,col="red")
 rect(15, 0,   18, 10, col = "white", border = "white", lwd = 2)
 dev.off()
 
-png("dport.png")
-plot(df$subsession.round_number[df$type==2],df$portype[df$type==2],ylim=c(0,80),type="l",col="black",lty=1,xaxt = "n",xlab="period",ylab = "disersion a-b")
+png("dport1.png")
+plot(df$subsession.round_number[df$type==2],df$portype[df$type==2],ylim=c(0,80),type="l",col="black",lty=1,xaxt = "n",xlab="period",ylab = "(a-b)^2")
 axis(1, at=c(1:15,18:32), labels=c(1:15,1:15))
 lines(df$subsession.round_number[df$type==1],df$portype[df$type==1],ylim=c(0,80),type="l",col="black",lty=2)
 lines(df$subsession.round_number[df$type==0],df$portype[df$type==0],ylim=c(0,80),type="l",col="black",lty=3)
 dev.off()
 
-png("dport.png")
-plot(df$subsession.round_number[df$type==2],df$portype[df$type==2],ylim=c(0,80),type="l",col="black",lty=1,xaxt = "n",xlab="period",ylab = "disersion a-b")
-axis(1, at=c(1:15,18:32), labels=c(1:15,1:15))
-lines(df$subsession.round_number[df$type==1],df$portype[df$type==1],ylim=c(0,80),type="l",col="black",lty=2)
-lines(df$subsession.round_number[df$type==0],df$portype[df$type==0],ylim=c(0,80),type="l",col="black",lty=3)
-dev.off()
 
-png("difport.png")
-plot(df$subsession.round_number[df$type==2],df$portdiftype[df$type==2],ylim=c(-20,20),type="l",col="black",lty=1,xaxt = "n",xlab="period",ylab = "disersion a-b")
+png("difport1.png")
+plot(df$subsession.round_number[df$type==2],df$portdiftype[df$type==2],ylim=c(-20,20),type="l",col="black",lty=1,xaxt = "n",xlab="period",ylab = "|a-b|")
 axis(1, at=c(1:15,18:32), labels=c(1:15,1:15))
 lines(df$subsession.round_number[df$type==1],df$portdiftype[df$type==1],col="black",lty=2)
 lines(df$subsession.round_number[df$type==0],df$portdiftype[df$type==0],col="black",lty=3)
 dev.off()
 
-png("holdings_gc.png")
+png("holdings_gc1.png")
 plot(df$subsession.round_number[df$type==2],df$agroup[df$type==2],ylim=c(0,40),type="l",col="gray",lty=1,xaxt = "n",xlab="period",ylab = "holdings type c")
 axis(1, at=c(1:15,18:32), labels=c(1:15,1:15))
 lines(df$subsession.round_number[df$type==2],df$bgroup[df$type==2],col="red")
 lines(df$subsession.round_number[df$type==2],df$cgroup[df$type==2],col="blue")
 dev.off()
 
-png("holdings_gb.png")
+png("holdings_gb1.png")
 plot(df$subsession.round_number[df$type==2],df$agroup[df$type==1],ylim=c(0,40),type="l",col="gray",lty=1,xaxt = "n",xlab="period",ylab = "holdings type b")
 axis(1, at=c(1:15,18:32), labels=c(1:15,1:15))
 lines(df$subsession.round_number[df$type==2],df$bgroup[df$type==1],col="red")
 lines(df$subsession.round_number[df$type==2],df$cgroup[df$type==1],col="blue")
 dev.off()
 
-png("holdings_ga.png")
+png("holdings_ga1.png")
 plot(df$subsession.round_number[df$type==2],df$agroup[df$type==0],ylim=c(0,40),type="l",col="gray",lty=1,xaxt = "n",xlab="period",ylab = "holdings type a")
 axis(1, at=c(1:15,18:32), labels=c(1:15,1:15))
 lines(df$subsession.round_number[df$type==2],df$bgroup[df$type==0],col="red")
